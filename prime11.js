bb3df854c065595277e760bbeba0c071254b3303
Satya2008 <satyanand807@gmail.com>
2022-12-08 20:55:33 +0530

    prime problem

---
+++ prime11.js
@@ -0,0 +1,13 @@
+prime problem...
+let num=10;
+let count=0;
+for(let i=0; i<=num; i+){
+if(num%i==0){
+count++;
+}
+}
+if (count==2){
+console.log("it is prime")
+}else{
+console.log("it is not prime")
+}
